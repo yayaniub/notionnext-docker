c131300c292345afbac7f20243226784f1ea33fe
yayaniub <106913111+yayaniub@users.noreply.github.com>
2023-11-04 10:11:10 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -47,7 +47,7 @@ const BLOG = {
   ],
   // 无衬线字体 例如'"LXGW WenKai"'
   FONT_SANS: [
-    // '"LXGW WenKai"',
+    '"LXGW WenKai"',
     '"PingFang SC"',
     '-apple-system',
     'BlinkMacSystemFont',
@@ -67,7 +67,7 @@ const BLOG = {
   ],
   // 衬线字体 例如'"LXGW WenKai"'
   FONT_SERIF: [
-    // '"LXGW WenKai"',
+    '"LXGW WenKai"',
     'Bitter',
     '"Noto Serif SC"',
     'SimSun',
